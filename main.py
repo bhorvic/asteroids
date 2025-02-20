083ba6dc1966e19d6b711f502f8779123cff051b
bhorvic <bhorvic@gmail.com>
2025-02-20 12:06:56 -0700

    first commit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +18,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,5 @@
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x, y)
+        super().__init__()
